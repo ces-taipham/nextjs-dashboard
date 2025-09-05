f7fd099cd6d7b397b6abc76d430eebf4c1a78c5a
Tai Pham <tai.pham@codeenginestudio.com>
2025-09-05 09:49:01 +0700

    Update few config

---
+++ config/env.ts
@@ -0,0 +1,30 @@
+import * as z from 'zod';
+
+const createEnv = () => {
+  const EnvSchema = z.object({
+    SUPABASE_URL: z.string(),
+    SUPABASE_ANON_KEY: z.string().optional().default(''),
+  });
+
+  const envVars = {
+    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  };
+
+  const parsedEnv = EnvSchema.safeParse(envVars);
+
+  if (!parsedEnv.success) {
+    throw new Error(
+      `Invalid env provided.
+  The following variables are missing or invalid:
+  ${Object.entries(parsedEnv.error.flatten().fieldErrors)
+    .map(([k, v]) => `- ${k}: ${v}`)
+    .join('\n')}
+  `,
+    );
+  }
+
+  return parsedEnv.data ?? {};
+};
+
+export const env = createEnv();
